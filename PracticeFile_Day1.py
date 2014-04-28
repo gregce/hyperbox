f63ed43c290e25d459776526b48af6b2403dc3a1
gregce <gregce@gmail.com>
2014-04-27 22:07:09 -0400

    Began list exercises

--- PracticeFile_Day1.py
+++ PracticeFile_Day1.py
@@ -29,6 +29,16 @@ def testpringf(s,s1):
     #a tuple on the right (a tuple is made of values separated by commas, typically grouped inside parenthesis):
     return test
 
+def listbuild(listy,itemtoadd):
+    listy.append(itemtoadd)
+    return list
+
+#listy = ['test1','test2']
+#listbuild(listy,'test3')
+#print listy
+
+#One common pattern is to start a list a the empty list [], then use append() or extend() to add elements to it:
+
 #Standard boilerplate to call main() function to begin 
 #By doing the main check, you can have that code only execute when you want to run the module as a program and not have it execute when someone just wants to import your module and call your functions themselves.
 if __name__ == '__main__':
@@ -57,3 +67,31 @@ if __name__ == '__main__':
     #s.replace('old', 'new') -- returns a string where all occurrences of 'old' have been replaced by 'new'
     #s.split('delim') -- returns a list of substrings separated by the given delimiter. The delimiter is not a regular expression, it's just text. 'aaa,bbb,ccc'.split(',') -> ['aaa', 'bbb', 'ccc']. As a convenient special case s.split() (with no arguments) splits on all whitespace chars.
     #s.join(list) -- opposite of split(), joins the elements in the given list together using the string as the delimiter. e.g. '---'.join(['aaa', 'bbb', 'ccc']) -> aaa---bbb---ccc
+
+    
+    #Lists
+    #Assignment with an = on lists does not make a copy. 
+    #Instead, assignment makes the two variables point to the one list in memory.
+
+    #b = colors   ## Does not copy the list
+    
+    #FOR operator for collections
+    #Python's *for* and *in* constructs are extremely useful, and the first use of them we'll see is with lists. The *for* construct -- for var in list -- is an easy way to look at each element in a list (or other collection). 
+    #Do not add or remove from the list during iteration.
+   
+    #Never manually iterate over a collection, use the FOR/IN construct in Python
+    
+    #for i in range(100) is one way to iterate, while i < NUMBER: i=i+1
+    
+    #Common list methods include
+    
+    #list = ['larry', 'curly', 'moe']
+    #list.append('shemp')         ## append elem at end
+    #list.insert(0, 'xxx')        ## insert elem at index 0
+    #list.extend(['yyy', 'zzz'])  ## add list of elems at end
+    #print list  ## ['xxx', 'larry', 'curly', 'moe', 'shemp', 'yyy', 'zzz']
+    #print list.index('curly')    ## 2
+    
+    #list.remove('curly')         ## search and remove that element
+    #list.pop(1)                  ## removes and returns 'larry'
+    #print list  ## ['xxx', 'moe', 'shemp', 'yyy', 'zzz']
