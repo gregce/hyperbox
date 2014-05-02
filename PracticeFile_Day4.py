8d020e29871b3a18355de2bfdb3b8743ecfb9759
gregce <gregce@gmail.com>
2014-05-01 23:01:33 -0400

    Added some py files but still in technical debt..

---
+++ PracticeFile_Day4.py
@@ -0,0 +1,11 @@
+###################
+# 2014-04-30 --
+# Continue to Reacquaint with basic python functionality.
+# Review common concepts to ensure familiarity. Learn some new concepts for the list.sort() operator.
+
+
+#It's most common to pass a list into the sorted() function, but in fact it can take as input any sort of iterable collection. The older list.sort() 
+#method is an alternative detailed below. The sorted() function seems easier to use compared to sort(), so I recommend using sorted().
+#
+#The sorted() function can be customized though optional arguments. The sorted() optional argument reverse=True, e.g. sorted(list, reverse=True), makes it sort backwards.
+
